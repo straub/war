5551ea42c6a1b643f0d345511fd7e9f371cc28d0
David Straub <david-straub@straubdev.com>
2013-02-10 19:38:38 -0500

    Balance tweaks, refactoring, support accepting random seed from query string for reproducability.

--- public/js/war.js
+++ public/js/war.js
@@ -1,109 +1,4 @@
 
-(function() {
-    var lastTime = 0;
-    var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelAnimationFrame =
-          window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
-    }
- 
-    // Temporarily disabling the realRequestAnimationFrame
-    // because it wasn't giving good performance.
-    /*if (!window.requestAnimationFrame)*/
-        window.requestAnimationFrame = function(callback, element) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
-              timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
- 
-    if (!window.cancelAnimationFrame)
-        window.cancelAnimationFrame = function(id) {
-            clearTimeout(id);
-        };
-}());
-
-// From http://baagoe.com/en/RandomMusings/javascript/
-// Johannes Baagøe <baagoe@baagoe.com>, 2010
-function Mash() {
-  var n = 0xefc8249d;
-
-  var mash = function(data) {
-    data = data.toString();
-    for (var i = 0; i < data.length; i++) {
-      n += data.charCodeAt(i);
-      var h = 0.02519603282416938 * n;
-      n = h >>> 0;
-      h -= n;
-      h *= n;
-      n = h >>> 0;
-      h -= n;
-      n += h * 0x100000000; // 2^32
-    }
-    return (n >>> 0) * 2.3283064365386963e-10; // 2^-32
-  };
-
-  mash.version = 'Mash 0.9';
-  return mash;
-}
-
-// From http://baagoe.com/en/RandomMusings/javascript/
-function Alea() {
-  return (function(args) {
-    // Johannes Baagøe <baagoe@baagoe.com>, 2010
-    var s0 = 0;
-    var s1 = 0;
-    var s2 = 0;
-    var c = 1;
-
-    if (!args.length) {
-      args = [+new Date()];
-    }
-    var mash = Mash();
-    s0 = mash(' ');
-    s1 = mash(' ');
-    s2 = mash(' ');
-
-    for (var i = 0; i < args.length; i++) {
-      s0 -= mash(args[i]);
-      if (s0 < 0) {
-        s0 += 1;
-      }
-      s1 -= mash(args[i]);
-      if (s1 < 0) {
-        s1 += 1;
-      }
-      s2 -= mash(args[i]);
-      if (s2 < 0) {
-        s2 += 1;
-      }
-    }
-    mash = null;
-
-    var random = function() {
-      var t = 2091639 * s0 + c * 2.3283064365386963e-10; // 2^-32
-      s0 = s1;
-      s1 = s2;
-      s2 = t - (c = t | 0);
-      return s2;
-    };
-    random.uint32 = function() {
-      return random() * 0x100000000; // 2^32
-    };
-    random.fract53 = function() {
-      return random() +
-        (random() * 0x200000 | 0) * 1.1102230246251565e-16; // 2^-53
-    };
-    random.version = 'Alea 0.9';
-    random.args = args;
-    return random;
-
-  } (Array.prototype.slice.call(arguments)));
-}
-
 // Set up namespace
 var app = app || {};
 (function( $, _, window, document, undefined ) {
@@ -113,7 +8,10 @@ var self = this;
 
 this.frequency = {};
 
-this.random = new Alea(Math.random(), Math.random(), Math.random());
+// Support seed being passed by query string,
+// for reproducability.
+this.randomSeed = getParameterByName("seed") || Math.random();
+this.random = new Alea(app.randomSeed);
 
 this.recordTiming = false;
 
@@ -808,7 +706,7 @@ var Entity = app.Entity = Backbone.Model.extend(
     },
 
     wander: function () {
-        var randomizeSign = function(x){ return x * Math.pow(-1, Math.round(Math.random()*10)); };
+        var randomizeSign = function(x){ return x * Math.pow(-1, Math.round(app.random()*10)); };
         var tempJerk = Vector.Random(2).to3D().map(randomizeSign).toUnitVector();
 
         if(this.radsToDegrees(this.get("vel").angleFrom(tempJerk)) < 90)
@@ -1123,14 +1021,18 @@ Entity.teamDefaults = {
     },
     // Health
     blue: {
-        health: 150,
-        maxHealth: 150,
-        healRate: 0.15,
+        health: 90,
+        maxHealth: 90,
+        healRate: 0.5,
+        muzzleVel: 6,
+
+        neighborhoodRadius: 300,
 
         avoidanceRange: 150,
-        cohesionRange: 150,
+        cohesionRange: 250,
         cohesionStrength: 0.3,
-        separationRange: 25
+        separationRange: 25,
+        alignmentStrength: 2.5
     },
     // Sniper
     orange: {
@@ -1385,7 +1287,7 @@ var EntityCollection = Backbone.Collection.extend({
                 accel: Vector.Zero(3),
                 jerk: Vector.Zero(3),
                 color: rand <= 0.25 ? "blue" : rand <= 0.5 ? "red" : rand <= 0.75 ? "green" : "orange",
-                pos: Vector.create([Math.round(Math.random()*app.viewportWidth), Math.round(Math.random()*app.viewportHeight), 0])
+                pos: Vector.create([Math.round(app.random()*app.viewportWidth), Math.round(app.random()*app.viewportHeight), 0])
             }, obj));
         
         new EntityView({ model: entity });
@@ -1562,7 +1464,7 @@ var AppView = Backbone.View.extend({
     // This code launches projectiles in random directions
     // from the location of a mouse click.
     /*handleClick: function (e) {
-        var randomizeSign = function(x){ return x * Math.pow(-1, Math.round(Math.random()*10)); };
+        var randomizeSign = function(x){ return x * Math.pow(-1, Math.round(app.random()*10)); };
         var projectiles = [];
         for(var i = 0; i < 10; i++){
             projectiles.push(new Proj({
@@ -1762,5 +1664,122 @@ this.toggleFullscreen = function (element) {
     }
 };
 
+// From http://stackoverflow.com/a/901144
+function getParameterByName(name) {
+  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+  var regexS = "[\\?&]" + name + "=([^&#]*)";
+  var regex = new RegExp(regexS);
+  var results = regex.exec(window.location.search);
+  if(!results)
+    return "";
+  else
+    return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
 // End namespace setup
 }).call(app, jQuery, window._, window, document);
+
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame =
+          window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+ 
+    // Temporarily disabling the realRequestAnimationFrame
+    // because it wasn't giving good performance.
+    /*if (!window.requestAnimationFrame)*/
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+ 
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
+// From http://baagoe.com/en/RandomMusings/javascript/
+// Johannes Baagøe <baagoe@baagoe.com>, 2010
+function Mash() {
+  var n = 0xefc8249d;
+
+  var mash = function(data) {
+    data = data.toString();
+    for (var i = 0; i < data.length; i++) {
+      n += data.charCodeAt(i);
+      var h = 0.02519603282416938 * n;
+      n = h >>> 0;
+      h -= n;
+      h *= n;
+      n = h >>> 0;
+      h -= n;
+      n += h * 0x100000000; // 2^32
+    }
+    return (n >>> 0) * 2.3283064365386963e-10; // 2^-32
+  };
+
+  mash.version = 'Mash 0.9';
+  return mash;
+}
+
+// From http://baagoe.com/en/RandomMusings/javascript/
+function Alea() {
+  return (function(args) {
+    // Johannes Baagøe <baagoe@baagoe.com>, 2010
+    var s0 = 0;
+    var s1 = 0;
+    var s2 = 0;
+    var c = 1;
+
+    if (!args.length) {
+      args = [+new Date()];
+    }
+    var mash = Mash();
+    s0 = mash(' ');
+    s1 = mash(' ');
+    s2 = mash(' ');
+
+    for (var i = 0; i < args.length; i++) {
+      s0 -= mash(args[i]);
+      if (s0 < 0) {
+        s0 += 1;
+      }
+      s1 -= mash(args[i]);
+      if (s1 < 0) {
+        s1 += 1;
+      }
+      s2 -= mash(args[i]);
+      if (s2 < 0) {
+        s2 += 1;
+      }
+    }
+    mash = null;
+
+    var random = function() {
+      var t = 2091639 * s0 + c * 2.3283064365386963e-10; // 2^-32
+      s0 = s1;
+      s1 = s2;
+      s2 = t - (c = t | 0);
+      return s2;
+    };
+    random.uint32 = function() {
+      return random() * 0x100000000; // 2^32
+    };
+    random.fract53 = function() {
+      return random() +
+        (random() * 0x200000 | 0) * 1.1102230246251565e-16; // 2^-53
+    };
+    random.version = 'Alea 0.9';
+    random.args = args;
+    return random;
+
+  } (Array.prototype.slice.call(arguments)));
+}
